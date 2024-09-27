69618f5ca2657057fc7311cf998dfba60f2d667b
Sira <sgonzmad@myuax.com>
2024-09-27 10:37:27 +0200

    quitar imports no usados

--- app/src/main/java/com/example/feedback1/MainActivity.kt
+++ app/src/main/java/com/example/feedback1/MainActivity.kt
@@ -3,16 +3,12 @@ package com.example.feedback1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
